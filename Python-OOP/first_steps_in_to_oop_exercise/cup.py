b74615bef4e7c8fb94ea3fee5c993d2c99377dd9
Петър Попов <popovstz@gmail.com>
2024-02-24 13:55:23 +0200

    problem 4

---
+++ Python-OOP/first_steps_in_to_oop_exercise/cup.py
@@ -0,0 +1,17 @@
+class Cup:
+    def __init__(self, size: int, quantity: int) -> None:
+        self.size = size
+        self.quantity = quantity
+
+    def fill(self, quantity: int) -> None:
+        if quantity + self.quantity <= self.size:
+            self.quantity += quantity
+
+    def status(self) -> int:
+        return self.size - self.quantity
+
+# cup = Cup(100, 50)
+# print(cup.status())
+# cup.fill(40)
+# cup.fill(20)
+# print(cup.status())
