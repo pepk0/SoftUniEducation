548e66c534429caac5ddf1bcabcb099f5d6702cf
Петър Попов <popovstz@gmail.com>
2024-02-25 13:31:16 +0200

    problem 1 and 2

---
+++ Python-OOP/classes_and_objects_exercise/time.py
@@ -0,0 +1,32 @@
+class Time:
+    max_hours: int = 23
+    max_minutes: int = 59
+    max_seconds: int = 59
+
+    def __init__(self, hours: int, minutes: int, seconds: int) -> None:
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours: int, minutes: int, seconds: int) -> None:
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def get_time(self) -> int:
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+    def next_second(self) -> str:
+        self.seconds += 1
+        if self.seconds > self.max_seconds:
+            self.seconds = 0
+            self.minutes += 1
+
+        if self.minutes > self.max_minutes:
+            self.minutes = 0
+            self.hours += 1
+
+        if self.hours > self.max_hours:
+            self.hours = 0
+
+        return self.get_time()
