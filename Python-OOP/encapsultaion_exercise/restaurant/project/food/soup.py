cfa3cce95f6fc30494f9c1968b6442e1838a14a7
Петър Попов <popovstz@gmail.com>
2024-03-04 20:48:46 +0200

    problem 4

---
+++ Python-OOP/encapsultaion_exercise/restaurant/project/food/soup.py
@@ -0,0 +1,6 @@
+from project.food.starter import Starter
+
+
+class Soup(Starter):
+    def __init__(self, name: str, price: float, grams: float) -> None:
+        super().__init__(name, price, grams)
