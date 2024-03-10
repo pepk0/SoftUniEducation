b21fec87298f72674a93a30e73aaadf6411e0ae4
Петър Попов <popovstz@gmail.com>
2024-03-10 13:24:14 +0200

    problem 2

---
+++ Python-OOP/polymorphism_and_abstraction_exercise/groups.py
@@ -0,0 +1,34 @@
+class Person:
+    def __init__(self, name: str, surname: str) -> None:
+        self.name = name
+        self.surname = surname
+
+    def __repr__(self) -> str:
+        return f"{self.name} {self.surname}"
+
+    def __add__(self, other):
+        return Person(self.name, other.surname)
+
+
+class Group:
+    def __init__(self, name: str, people: list) -> None:
+        self.name = name
+        self.people = people
+
+    def __len__(self) -> int:
+        return len(self.people)
+
+    def __add__(self, other):
+        return Group(f"{self.name} {other.name}",
+                     self.people + other.people)
+
+    def __repr__(self) -> str:
+        people = ", ".join(str(person) for person in self.people)
+        return f"Group {self.name} with members {people}"
+
+    def __iter__(self):
+        for index, individual in enumerate(self.people):
+            yield f"Person {index}: {individual}"
+
+    def __getitem__(self, index) -> str:
+        return f"Person {index}: {self.people[index]}"
