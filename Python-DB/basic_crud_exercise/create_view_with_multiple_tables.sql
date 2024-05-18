8e9dfd5f5a74e56612b55e1c876a155cd67df146
Петър Попов <popovstz@gmail.com>
2024-05-18 22:34:42 +0300

    exercise 20

---
+++ Python-DB/basic_crud_exercise/create_view_with_multiple_tables.sql
@@ -0,0 +1,19 @@
+CREATE VIEW view_addresses AS
+	SELECT
+		CONCAT(
+			e.first_name,
+			' ',
+			e.last_name
+		) AS full_name,
+		e.department_id,
+		CONCAT(
+			a.number,
+			' ',
+			a.street
+		) AS address
+	FROM
+		employees AS e, addresses AS a
+	WHERE
+		e.address_id = a.id
+	ORDER BY 
+		address;
